29b15244d995d75691ff71e29f14c311a3fdd6cf
leonawicz <matt.rstat@gmail.com>
2017-01-04 16:23:34 -0700

    Add cumulative plot, hide plot for testing, refactor code, begin to address issues with NULL click and brush values with plot 1 resetting.

--- mod.R
+++ mod.R
@@ -4,8 +4,11 @@ dbmodUI <- function(id, tab_name){
     tabItem(tabName=tab_name,
       fluidRow(
         column(4, leafletOutput(ns("Map"), width="100%")),
-        column(8, plotOutput(ns("plot1"), click=ns("plot1_click"), dblclick=ns("plot1_dblclick"), hover=ns("plot1_hover"),
-                             brush=brushOpts(id=ns("plot1_brush"), resetOnNew=TRUE)))
+        column(8,
+               plotOutput(ns("plot_ts1"), click=ns("plot1_click"), dblclick=ns("plot1_dblclick"), hover=ns("plot1_hover"),
+                             brush=brushOpts(id=ns("plot1_brush"), resetOnNew=TRUE))#,
+               #conditionalPanel(condition=sprintf("input['%s'] == true", ns("cumulative")), plotOutput(ns("plot_ts2")))
+        )
       ),
       br(),
       fluidRow(
@@ -29,7 +32,7 @@ dbmodUI <- function(id, tab_name){
   
 }
 
-dbmod <- function(input, output, session, xdata, variable, stat, allRows, clickExclude, alpha, showLines, jitterPoints){
+dbmod <- function(input, output, session, xdata, variable, stat, alpha, showLines, jitterPoints){
   ns <- session$ns
   
   source("utils.R", local=TRUE)
@@ -53,12 +56,8 @@ dbmod <- function(input, output, session, xdata, variable, stat, allRows, clickE
   
   # Toggle points that are clicked
   observeEvent(input$plot1_click, {
-    if(clickExclude()){
-      res <- nearPoints(xdata(), input$plot1_click, allRows=TRUE)
-      rv_plot1$keeprows <- xor(rv_plot1$keeprows, res$selected_)
-    } else {
-      rv_plot1$keeprows <- rep(TRUE, nrow(xdata()))
-    }
+    res <- nearPoints(xdata(), input$plot1_click, allRows=TRUE)
+    rv_plot1$keeprows <- xor(rv_plot1$keeprows, res$selected_)
   })
   
   # Toggle points that are brushed, when button is clicked
@@ -78,18 +77,42 @@ dbmod <- function(input, output, session, xdata, variable, stat, allRows, clickE
     if(length(rv_plot1$keeprows) != nrow(xdata())) return()
     filter(xdata(), rv_plot1$keeprows)
   })
-  exclude <- reactive({ filter(xdata(), !rv_plot1$keeprows) })
+  exclude <- reactive({
+    req(rv_plot1$keeprows)
+    if(length(rv_plot1$keeprows) != nrow(xdata())) return()
+    filter(xdata(), !rv_plot1$keeprows)
+  })
   colorvec <- reactive({ if(is.null(colorby())) NULL else tolpal(length(unique(keep()[[colorby()]]))) })
   
-  output$plot1 <- renderPlot({
+  output$plot_ts1 <- renderPlot({
     if(nrow(xdata())==0 | nrow(xdata())!=length(rv_plot1$keeprows) | xdata()$Var[1]!=variable) return()
     if(jitterPoints()) pos <- position_jitter(w=0.2, h=0) else pos <- "identity"
     
-    g <- ggplot(data=keep(), aes_string("Year", stat, colour=colorby())) 
+    g <- ggplot(data=keep(), aes_string("Year", stat, colour=colorby()))
     if(showLines()) g <- g + geom_line(aes(group=interaction(GBM, RCP, Model, Region, Var, Vegetation)), alpha=alpha())
     
     g <- g + geom_point(size=3, alpha=alpha(), position=pos) +
       geom_point(data=exclude(), shape=21, fill=NA, color="black", alpha=0.25) +
+      coord_cartesian(xlim=rv_plot1$x, ylim=rv_plot1$y) + theme_bw(base_size = 14)
+    
+    if(!is.null(colorvec())) g <- g + scale_colour_manual(values=colorvec())
+    if(input$facetby!="") g <- g + facet_wrap(as.formula(paste0("~", input$facetby)), scales=input$facet_scales)
+    g + plottheme
+  })
+  
+  output$plot_ts2 <- renderPlot({
+    return()
+    if(nrow(xdata())==0 | nrow(xdata())!=length(rv_plot1$keeprows) | xdata()$Var[1]!=variable) return()
+    if(jitterPoints()) pos <- position_jitter(w=0.2, h=0) else pos <- "identity"
+    
+    grp <- c("GBM", "RCP", "Model", "Region", "Var", "Vegetation")
+    d_keep <- group_by_(keep(), .dots=grp) %>% mutate_(`Cumulative_total`=lazyeval::interp(~cumsum(x), x=as.name(stat)))
+    statname <- "Cumulative_total"
+    
+    g <- ggplot(data=d_keep, aes_string("Year", statname, colour=colorby()))
+    if(showLines()) g <- g + geom_line(aes(group=interaction(GBM, RCP, Model, Region, Var, Vegetation)), alpha=alpha())
+    
+    g <- g + geom_point(size=3, alpha=alpha(), position=pos) +
       coord_cartesian(xlim=rv_plot1$x, ylim=rv_plot1$y) + theme_bw(base_size = 14)
     
     if(!is.null(colorvec())) g <- g + scale_colour_manual(values=colorvec())
@@ -117,23 +140,29 @@ dbmod <- function(input, output, session, xdata, variable, stat, allRows, clickE
   })
   
   output$Selected_obs <- DT::renderDataTable({
-    x <- if(is.null(input$plot1_brush)) nearPoints(xdata(), input$plot1_click, allRows=allRows()) else
-      brushedPoints(xdata(), input$plot1_brush, allRows=allRows())
+    if(is.null(input$plot1_brush) && is.null(input$plot1_click)){
+      x <- slice(xdata(), 0)
+    } else if(is.null(input$plot1_brush)){
+      x <- nearPoints(xdata(), input$plot1_click, allRows=TRUE)
+    } else {
+      x <- brushedPoints(xdata(), input$plot1_brush, allRows=TRUE)
+    }
     
     if(nrow(x)==0) return(
       DT::datatable(x, options=list(
-      lengthMenu=list(c(5, 10, 25), c('5', '10', '25')), pageLength=5, emptyTable="No observations selected in plot", searching=FALSE)) %>%
+      lengthMenu=list(c(5, 10, 25), c('5', '10', '25')), pageLength=5, searching=FALSE)) %>%
       formatStyle(columns="Var", backgroundColor="white", target='row')
     )
     
-    if(!"selected_" %in% names(x)) x <- mutate(x, selected_=TRUE)
-    x <- mutate(x, selected_=paste0(x[[input$colorby]], "_", x$selected_))
+    x <- mutate(x, included_=rv_plot1$keeprows) %>% filter(selected_) %>% mutate(selected_=NULL)
+    x <- mutate(x, included_=paste0(x[[input$colorby]], "_", x$included_))
+
     clrs <- tableRowColors(x, input$colorby, colorvec(), "25")
     
     DT::datatable(x, options=list(
       lengthMenu=list(c(5, 10, 25), c('5', '10', '25')), pageLength=5, searching=FALSE,
       columnDefs=list(list(visible=FALSE, targets=ncol(x))))) %>%
-      formatStyle(columns="selected_", backgroundColor=clrs, target='row')
+      formatStyle(columns="included_", backgroundColor=clrs, target='row')
   })
 
 }
