6cd5c4b12a89203ee6d00dfbdcc6ad2499fea85a
leonawicz <matt.rstat@gmail.com>
2017-01-06 16:33:11 -0700

    Change variable name

--- global.R
+++ global.R
@@ -7,7 +7,7 @@ load("appData.RData")
 
 action_btn_style <- "color: #fff; background-color: #337ab7; border-color: #2e6da4"
 groupby_vars <- c("", "GBM", "RCP", "Model", "Region", "Vegetation")
-pooled_vars <- c("Average observations", "Unique observations")
+pooled_options <- c("Average observations", "Unique observations")
 axis_scales <- c("Fixed"="fixed", "Free"="free", "Free X"="free_x", "Free Y"="free_y")
 
 source("mod.R")
