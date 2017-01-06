8b9d437e0109d4b57335881bfdbb64aa037dfe7f
leonawicz <matt.rstat@gmail.com>
2017-01-06 11:57:12 -0700

    Only one variable used at a time, add option to pool unspecified factors by averaging or leaving displayed as unique obs in plots.

--- global.R
+++ global.R
@@ -6,7 +6,8 @@ library(DT)
 load("appData.RData")
 
 action_btn_style <- "color: #fff; background-color: #337ab7; border-color: #2e6da4"
-groupby_vars <- c("", "GBM", "RCP", "Model", "Region", "Var", "Vegetation")
+groupby_vars <- c("", "GBM", "RCP", "Model", "Region", "Vegetation")
+pooled_vars <- c("Average observations", "Unique observations")
 axis_scales <- c("Fixed"="fixed", "Free"="free", "Free X"="free_x", "Free Y"="free_y")
 
 source("mod.R")
