c3009c77726713c86a8ac2473dbc88ab79dab917
leonawicz <matt.rstat@gmail.com>
2017-01-23 13:23:10 -0700

    Refactor main mod UI so that reactive inputs and outputs associated with the UI side of a module can be separated and relocated more arbitrarily within the overall UI.

--- modules/main/mod.R
+++ modules/main/mod.R
@@ -1,15 +1,26 @@
 mainModUI <- function(id, tab_name){
   ns <- NS(id)
+  main <- c("Annual time series", "Aggregate distribution", "Decadal change")
   tabItem(tabName=tab_name,
     fluidRow(
-      column(8, tsModUI(id=ns("mod_ts"),
-        titles=c("Raw Observations", "Cumulative"), values=c("annual", "cumulative"), main="Annual time series")),
-      column(4, denModUI(id=ns("mod_den"),
-        titles=c("Density", "Histogram"), values=c("density", "histogram"), main="Aggregate distribution"))
+      column(8, tsModUI(id=ns("mod_ts"), type="plot",
+        titles=c("Raw Observations", "Cumulative"), values=c("annual", "cumulative"), main=main[1])),
+      column(4, denModUI(id=ns("mod_den"), type="plot",
+        titles=c("Density", "Histogram"), values=c("density", "histogram"), main=main[2]))
     ),
     fluidRow(
-      decModUI(id=ns("mod_dec"),
-        titles=c("Observations", "Averages"), values=c("dec_boxplot", "dec_barplot"), main="Decadal change"))
+      decModUI(id=ns("mod_dec"), type="plot",
+        titles=c("Observations", "Averages"), values=c("dec_boxplot", "dec_barplot"), main=main[3])),
+    fluidRow(
+      box(
+        tabBox(
+          tabPanel(main[3], decModUI(id=ns("mod_dec"), type="inputs", main=main[3]), value=ns("decadal")),
+          tabPanel(main[2], denModUI(id=ns("mod_den"), type="inputs", main=main[2]), value=ns("aggregate")),
+          tabPanel(main[1], tsModUI(id=ns("mod_ts"), type="inputs", main=main[1]), value=ns("annual")),
+          ns(id="tb_inputs"), selected=ns("annual"), title="Plot-specific controls and options", width=12, side="right"),
+        title="Plot settings", status="primary", solidHeader=TRUE, width=12, collapsible=TRUE, collapsed=TRUE
+      )
+    )
   )
 }
 
