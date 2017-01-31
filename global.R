b5577dfa3221775115e1b9b49167b79ac72d4dae
leonawicz <matt.rstat@gmail.com>
2017-01-31 14:36:36 -0700

    Add toasts, remove GBM.

--- global.R
+++ global.R
@@ -2,6 +2,7 @@ library(leaflet)
 library(shiny)
 library(shinyBS)
 library(shinydashboard)
+library(shinytoastr)
 library(DT)
 
 library(rgdal)
@@ -12,7 +13,7 @@ library(ggplot2)
 load("appData.RData")
 
 action_btn_style <- "margin: 10px 15px 10px 15px; width: 200px"
-groupby_vars <- c("", "GBM", "RCP", "Model", "Region", "Vegetation")
+groupby_vars <- c("", "RCP", "Model", "Region", "Vegetation")
 pooled_options <- c("Average observations", "Unique observations")
 axis_scales <- c("Fixed"="fixed", "Free"="free", "Free X"="free_x", "Free Y"="free_y")
 
