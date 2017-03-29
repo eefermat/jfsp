0e88c511935eec1d00c06d94f42c4f86c12e96fa
leonawicz <matt.rstat@gmail.com>
2017-03-29 14:14:48 -0600

    Update text

--- global.R
+++ global.R
@@ -36,9 +36,8 @@ source("modules/plots/mod_plots.R")
 
 #enableBookmarking(store="server") # not yet available on shinyapps.io
 temptext <- p(em("This website was developed as part of project (#16-1-01-18) funded by the Joint Fire Science Program.
-If you would be interested in participating in a more in-depth discussion in late Feb-early Mar about the design/utility
-of this website and to explore future fire management options please fill out", 
-              a("this doodle poll", href="http://doodle.com/poll/74ktffkxqnvnmevc", target="_blank")))
+If you would be interested in participating in an interview to guide the direction of the management scenarios implemented
+as part of this work, please contact the project PI, Courtney Schultz (courtney.schultz@colostate.edu)"))
 
 cat("Remainder global.R time:\n")
 print(proc.time() - ptm)
