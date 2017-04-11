b2028d63612a8a95916d024af43326570bb6c336
leonawicz <matt.rstat@gmail.com>
2017-04-11 16:15:19 -0600

    Update button style.

--- global.R
+++ global.R
@@ -22,7 +22,7 @@ cat("Total data load time:\n")
 print(proc.time() - ptm)
 
 ptm <- proc.time()
-action_btn_style <- "margin: 10px 15px 10px 15px; width: 200px"
+action_btn_style <- "color: black; margin: 10px 15px 10px 15px; width: 200px;"
 groupby_vars <- c("", "RCP", "GCM"="Model", "Fire Mgmt Zone"="Region", "Vegetation")
 pooled_options <- c("Average observations", "Unique observations")
 axis_scales <- c("Fixed"="fixed", "Free"="free", "Free X"="free_x", "Free Y"="free_y")
